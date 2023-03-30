74d52f2d5bec87124b3a0e292b62a3018ff823b0
bannikovilea <92776171+bannikovilea@users.noreply.github.com>
2023-03-30 12:20:57 +0500

    Configure GitHub actions for mongo tests (#2)
    * Test with real mongo
    
    * Remove msbuild
    
    * test on linux

--- Source/EasyNetQ.Scheduler.Mongo.Tests/ScheduleRepositoryTests.cs
+++ Source/EasyNetQ.Scheduler.Mongo.Tests/ScheduleRepositoryTests.cs
@@ -1,72 +1,89 @@
 using System;
 using System.Text;
+using MongoDB.Driver;
 using Xunit;
 
 namespace EasyNetQ.Scheduler.Mongo.Tests
 {
-    public class ScheduleRepositoryTests
+    public class ScheduleRepositoryTests : IClassFixture<SchedulesDatabaseFixture>
     {
         private readonly ScheduleRepository scheduleRepository;
+        private readonly SchedulesDatabaseFixture databaseFixture;
 
-        public ScheduleRepositoryTests()
+        public ScheduleRepositoryTests(SchedulesDatabaseFixture databaseFixture)
         {
-            var configuration = new ScheduleRepositoryConfiguration
-                {
-                    ConnectionString = "mongodb://localhost:27017/?w=1&readPreference=primary&uuidRepresentation=csharpLegacy",
-                    CollectionName = "Schedules",
-                    DatabaseName = "EasyNetQ",
-                    DeleteTimeout = TimeSpan.FromMinutes(5),
-                    PublishTimeout = TimeSpan.FromSeconds(60),
-                };
-            scheduleRepository = new ScheduleRepository(configuration, () => DateTime.UtcNow);
+            this.databaseFixture = databaseFixture;
+            scheduleRepository = new ScheduleRepository(databaseFixture.Configuration, () => DateTime.UtcNow);
         }
 
-        [Fact(Skip = "Required a database")]
+        [Fact]
         public void Should_be_able_to_store_a_schedule()
         {
-            scheduleRepository.Store(new Schedule
-                {
-                    BindingKey = "abc",
-                    CancellationKey = "bcd",
-                    WakeTime = new DateTime(2011, 5, 18),
-                    InnerMessage = Encoding.UTF8.GetBytes("Hello World!"),
-                    Id = Guid.Empty,
-                    State = ScheduleState.Pending,
-                });
+            var schedule = new Schedule
+            {
+                BindingKey = "abc",
+                CancellationKey = "bcd",
+                WakeTime = new DateTime(2011, 5, 18),
+                InnerMessage = Encoding.UTF8.GetBytes("Hello World!"),
+                Id = Guid.NewGuid(),
+                State = ScheduleState.Pending,
+            };
+            scheduleRepository.Store(schedule);
+
+            var insertedDoc = databaseFixture.GetDocument(schedule.Id);
+
+            Assert.Equal(schedule.Id, insertedDoc["_id"].AsGuid);
+            Assert.Equal(schedule.CancellationKey, insertedDoc["CancellationKey"].AsString);
         }
 
-        [Fact(Skip = "Required a database")]
+        [Fact]
         public void Should_be_able_to_cancel_a_schedule()
         {
-            scheduleRepository.Cancel("bcd");
+            var cancellation = Guid.NewGuid().ToString();
+            databaseFixture.InitScheduleWithCancellation(cancellation);
+
+            scheduleRepository.Cancel(cancellation);
+
+            var count = databaseFixture.Collection.CountDocuments(d => d["CancellationKey"] == cancellation);
+            Assert.Equal(0, count);
         }
 
-        [Fact(Skip = "Required a database")]
+        [Fact]
         public void Should_be_able_to_get_messages()
         {
-            while (true)
-            {
-                var schedule = scheduleRepository.GetPending();
-                if(schedule == null)
-                    break;
-                Console.WriteLine("{0}, {1}, {2}",
-                                  schedule.BindingKey,
-                                  schedule.WakeTime,
-                                  Encoding.UTF8.GetString(schedule.InnerMessage));
-            }
+            var id = Guid.NewGuid();
+            var wakeTime = DateTime.MinValue.AddDays(5);
+            databaseFixture.InitializeOne(id, ScheduleState.Pending, wakeTime);
+
+            var schedule = scheduleRepository.GetPending();
+
+            Assert.Equal(id, schedule.Id);
         }
 
-        [Fact(Skip = "Required a database")]
+        [Fact]
         public void Should_be_able_to_handle_timeout()
         {
+            var id = Guid.NewGuid();
+            var publishingTime = DateTime.UtcNow.Add(-databaseFixture.Configuration.PublishTimeout);
+            databaseFixture.InitializeOne(id, ScheduleState.Publishing, publishingTime: publishingTime);
+
             scheduleRepository.HandleTimeout();
+
+            var actual = databaseFixture.GetDocument(id);
+            Assert.Equal(ScheduleState.Pending, actual["State"]);
         }
 
 
-        [Fact(Skip = "Required a database")]
+        [Fact]
         public void Should_be_able_to_mark_as_published()
         {
-            scheduleRepository.MarkAsPublished(Guid.Empty);
+            var id = Guid.NewGuid();
+            databaseFixture.InitializeOne(id);
+            scheduleRepository.MarkAsPublished(id);
+
+            var actual = databaseFixture.GetDocument(id);
+
+            Assert.Equal(ScheduleState.Published, actual["State"]);
         }
     }
 }
---
+++ Source/EasyNetQ.Scheduler.Mongo.Tests/SchedulesDatabaseFixture.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+namespace EasyNetQ.Scheduler.Mongo.Tests
+{
+
+    public class SchedulesDatabaseFixture : IDisposable
+    {
+        public readonly IMongoCollection<BsonDocument> Collection;
+        private readonly IMongoDatabase database;
+
+        public readonly ScheduleRepositoryConfiguration Configuration = new ScheduleRepositoryConfiguration
+        {
+            ConnectionString = "mongodb://localhost:27017/?w=1&readPreference=primary&uuidRepresentation=csharpLegacy",
+            CollectionName = "Schedules",
+            DatabaseName = "EasyNetQ",
+            DeleteTimeout = TimeSpan.FromMinutes(5),
+            PublishTimeout = TimeSpan.FromSeconds(60),
+        };
+
+        public SchedulesDatabaseFixture()
+        {
+            InitializeMongoConventions();
+            database = CreateDatabase(new MongoUrl(Configuration.ConnectionString), Configuration.DatabaseName);
+            Collection = database.GetCollection<BsonDocument>(Configuration.CollectionName);
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public void InitScheduleWithCancellation(string cancellation)
+        {
+            var schedule = new Schedule
+            {
+                CancellationKey = cancellation,
+                Id = Guid.NewGuid()
+            };
+            Collection.InsertOne(schedule.ToBsonDocument());
+        }
+
+        public void InitializeOne(Guid id, ScheduleState state = ScheduleState.Pending, DateTime? wakeTime = null, DateTime? publishingTime = null)
+        {
+            var schedule = new Schedule
+            {
+                Id = id,
+                State = state,
+                WakeTime = wakeTime ?? DateTime.UtcNow.AddDays(-1),
+                PublishingTime = publishingTime
+            };
+            Collection.InsertOne(schedule.ToBsonDocument());
+        }
+
+        public BsonDocument GetDocument(Guid id)
+            => database.GetCollection<BsonDocument>(Configuration.CollectionName)
+                .Find(d => d["_id"] == id)
+                .FirstOrDefault();
+
+        private static IEnumerable<BsonDocument> LoadBSONs()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "BSONs");
+            foreach (var fileName in Directory.GetFiles(path))
+            {
+                var content = File.ReadAllText(fileName);
+                var bson = BsonSerializer.Deserialize<BsonDocument>(content);
+                bson["_id"] = Guid.NewGuid();
+                yield return  bson;
+            }
+        }
+
+        private static IMongoDatabase CreateDatabase(MongoUrl connectionString, string databaseName)
+        {
+            var settings = MongoClientSettings.FromUrl(connectionString);
+            settings.ReadEncoding = new UTF8Encoding(false, false);
+            var client = new MongoClient(settings);
+            return client.GetDatabase(databaseName);
+        }
+
+        private static void InitializeMongoConventions()
+        {
+            ConventionRegistry.Register("ignoreIfNull", new ConventionPack {new IgnoreIfNullConvention(true)}, t => true);
+            ConventionRegistry.Register("ignoreExtraElements", new ConventionPack {new IgnoreExtraElementsConvention(true)}, t => true);
+        }
+    }
+}
